1176bf7f87393058adb0a1a9f5118b843a445568
Praveen <spraveenitpro@gmail.com>
2023-04-10 07:10:03 -0700

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,13 @@ import SliderSelect from './Components/SliderSelect'
 import TenureSelect from './Components/TenureSelect'
 
 function App() {
+    const [data, setData] = useState({
+        homeValue: 3000,
+        downPayment: 3000 * 0.2,
+        loanAmount: 3000 * 0.8,
+        loanTerm: 5,
+        interestRate: 5,
+    })
 
 
     return (
@@ -17,11 +24,11 @@ function App() {
             <Container maxWidth='xl' sx={{ marginTop: 4 }}>
                 <Grid container spacing={5} alignItems="center">
                     <Grid item xs={12} md={6}>
-                        <SliderSelect />
-                        <TenureSelect />
+                        <SliderSelect data={data} setData={setData} />
+                        <TenureSelect data={data} setData={setData} />
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Result />
+                        <Result data={data} setData={setData} />
                     </Grid>
                 </Grid>
             </Container>
--- src/Components/TenureSelect.jsx
+++ src/Components/TenureSelect.jsx
@@ -1,10 +1,31 @@
 import React from "react";
+import { InputLabel, MenuItem, FormControl, Select } from "@mui/material";
 
-function TenureSelect() {
+
+const TenureSelect = ({ data, setData }) => {
+
+    const handleChange = (event) => {
+        setData({ ...data, loanTerm: event.target.value })
+    }
     return (
-        <div>
-            <h1>TenureSelect</h1>
-        </div>
+        <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Tenure</InputLabel>
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={data.loanTerm}
+                label="Tenure"
+                defaultValue={5}
+                onChange={handleChange}
+            >
+                <MenuItem value={5}>5 years</MenuItem>
+                <MenuItem value={10}>10 years</MenuItem>
+                <MenuItem value={15}>15 years</MenuItem>
+                <MenuItem value={20}>20 years</MenuItem>
+                <MenuItem value={25}>25 years</MenuItem>
+            </Select>
+
+        </FormControl>
     )
 }
 
